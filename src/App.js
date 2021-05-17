fb72e76b98cb554238a05c3b75c035c291de6fe9
elementum-vita <82609134+elementum-vita@users.noreply.github.com>
2021-05-17 16:49:35 -0400

    Add files via upload
    At the begining, first upload from work.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  
+  render(){
+    const name = 'John Doe'
+
+    return (
+      <div className='App'>
+        <h1>Hello {name}</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
