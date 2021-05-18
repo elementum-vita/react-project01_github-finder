e28a24e7874d78b0837107f78c793264afd74edc
elementum-vita <82609134+elementum-vita@users.noreply.github.com>
2021-05-18 17:39:07 -0400

    Add files via upload
    update 18-MAY-2021 from work

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component {
-  
-  render(){
-    const name = 'John Doe'
-
+    render() { 
     return (
-      <div className='App'>
-        <h1>Hello {name}</h1>
+      <div className="App">
+      <Navbar />
       </div>
     );
   }
